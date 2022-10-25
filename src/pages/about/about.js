4bc63ffb4fa1b927d3e6317c5df134ee8826b3cf
Ixtiyor <ixtiyor6689@gmail.com>
2022-10-25 22:39:12 +0500

    some

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -4,10 +4,21 @@ import ASec1 from "../../components/about-sections/aSec1/aSec1";
 import FeatureSection from "../../components/about-sections/feature-sections/feature-section";
 import ContactUs from "../../components/home-sections/contacts/contacts";
 import Partners from "../../components/home-sections/partners/partners";
+import {Helmet} from "react-helmet";
+import img from "../../assets/imgs/photo.jpg";
 
 function About(props) {
     return (
         <div className="about">
+            <Helmet>
+                <meta charSet="utf-8"/>
+                <title>Safeeds Transport Inc</title>
+                <link rel="canonical" href="https://safeeds.us/"/>
+                <meta property="og:image" content={img}/>
+                <meta property="og:type" content="website"/>
+                <meta property="og:title"
+                      content="About desc"/>
+            </Helmet>
             <ASec1/>
             <FeatureSection/>
             <ContactUs/>
