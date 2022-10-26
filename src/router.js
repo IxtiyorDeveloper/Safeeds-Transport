4c7d4e51a3fb5e3f988367075f6cfd709a2bca41
Ixtiyor <ixtiyor6689@gmail.com>
2022-10-26 19:38:55 +0500

    done

---
+++ src/router.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Route} from 'react-router-dom';
+
+export default (
+    <Route>
+        <Route path='/'/>
+        <Route path='/about'/>
+        <Route path='/services'/>
+        <Route path='/blogs'/>
+        <Route path='/blogs/:id'/>
+        <Route path='/team'/>
+        <Route path='/contact'/>
+        <Route path='/help'/>
+        <Route path='/terms-and-conditions'/>
+        <Route path='/quote'/>
+        <Route path='/success'/>
+        <Route path='/team/:id'/>
+        <Route path='/login'/>
+        <Route path='/reset-password'/>
+        <Route path='/articles'/>
+        <Route path='/articles/:id'/>
+        <Route path='/articles/create'/>
+        <Route path='/satisfied-customers'/>
+        <Route path='/satisfied-customers/:id'/>
+        <Route path='/satisfied-customers/create'/>
+    </Route>
+);
