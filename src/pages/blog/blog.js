81f151bb1ed51e3098594600c434d1677ea66dc4
Фурсов Никита <ixtiyor6689@gmail.com>
2022-10-31 16:28:36 +0500

    dd

--- src/pages/blog/blog.js
+++ src/pages/blog/blog.js
@@ -51,7 +51,7 @@ function Blog(props) {
                 <meta property="og:description"
                       content="One of the leading car transportation companies all over the states. We ship all types of vehicles CARs SUVs. Motorcycles Trucks ATVs ☎️ (315)314 43 37."
                       data-react-helmet="true"/>
-                <meta property="og:url" content="https://majftyr.vercel.app/blogs/How-long-it-takes-to-deliver-my-vehicle"
+                <meta property="og:url" content="https://majtyr.vercel.app/blogs/How-long-it-takes-to-deliver-my-vehicle"
                       data-react-helmet="true"/>
                 <meta name="robots"
                       content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1"
@@ -104,10 +104,10 @@ function Blog(props) {
                       data-react-helmet="true"
                 />
                 <link rel="apple-touch-icon" href={logo100}/>
+                <link rel="canonical" href="https://majtyr.vercel.app/blogs/How-long-it-takes-to-deliver-my-vehicle" />
                 {/*<link rel="manifest"*/}
                 {/*      href="%PUBLIC_URL%/manifest.json"/>*/}
-                <title
-                    data-react-helmet="true">Safeeds Transport</title>
+                <title data-react-helmet="true">Safeeds Transport</title>
             </Helmet>
             <Spin spinning={isLoading || isLoading1}>
                 <Banner article={article}/>
